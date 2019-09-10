5afdf5ade5b52ff3dc500af2db70f3cf0b3be3d3
Cobb <hacobb@ku.edu>
2019-09-10 13:14:50 -0500

    Created Board.h file with get and set commands

--- Board.h
+++ Board.h
@@ -1,4 +1,14 @@
 #ifndef BOARD_H
 #define BOARD_H
-
 using namespace std;
+class Board
+{
+public:
+Board();
+~Board();
+void setPos(xPos,yPos,val);
+int getPos(xPos,yPos);
+private:
+int ship_Board[8][8];
+int shot_Board[8][8];
+
