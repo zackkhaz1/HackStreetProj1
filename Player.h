a2e82e1a2af47700765d6f66ea630793a9f23a51
Zack Khazraeinazmpour <zack_khaz@ku.edu>
2019-09-11 13:33:11 -0500

    edited class defs

--- Player.h
+++ Player.h
@@ -7,15 +7,14 @@ class Player
   {
   public:
     ~Player();
-    Player(int numShips);
-    void placeShips(int xPos, int yPos);
-    void fireShot(int xPos, int yPos);
-     
-  private:
-    Board ownBoard;
-    Board enemyBoard;
-    int ships = 0;
+    Player();
+    void placeShips(int xPos, int yPos); //Method to place ships on board
+    void fireShot(int xPos, int yPos); //Fires shots at enemy
 
+  private:
+    Board ownBoard; //Board designating where own ships are
+    Board enemyBoard; //Board showing where player has fired/hit enemy ships
+    int numShips = 0;
 
   };
 #endif
