5bffb3649e8dbbeddebb43cc0fd48088d4f3f0be
Zack Khazraeinazmpour <zack_khaz@ku.edu>
2019-09-11 12:34:46 -0500

    Edited ship constructor totake size as param, removed SetSize method

--- Ship.cpp
+++ Ship.cpp
@@ -1,18 +1,17 @@
 #include "Ship.h"
-Ship::Ship(){
-	size = 1;
+Ship::Ship(int ShipSize){
+	size = ShipSize;
 	orientation = 'h';
 	sunk = false;
 	hits = 0;
 }
 
-void Ship::setSize(int val){
-	size = val;
-}
+
 
 int Ship::getSize(){
 	return(size);
 }
+
 
 void Ship::setOrientation(char vh){
 	orientation = vh;
