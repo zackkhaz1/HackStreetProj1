a1ec22f6de1a952f2cd732748ed42683caee3fca
Zack Khazraeinazmpour <zack_khaz@ku.edu>
2019-09-11 12:37:42 -0500

    Added int to board method params

--- Board.h
+++ Board.h
@@ -6,10 +6,11 @@ class Board
 public:
 Board();
 ~Board();
-void setPos(xPos,yPos,val);
-char getPos(xPos,yPos);
+void setPos(int xPos,int yPos, int val);
+char getPos(int xPos, int yPos);
 void printBoard();
 private:
 char map[8][8];
 
-
+};
+#endif
