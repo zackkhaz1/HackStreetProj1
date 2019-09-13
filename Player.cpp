7d1c8d40d0b89d229f08159bcf122a3281f3892d
Justin Khounsombath <j795k105@ku.edu>
2019-09-13 12:16:22 -0500

    Changed player class to not take in user input for placeShips and constructor

--- Player.cpp
+++ Player.cpp
@@ -1,29 +1,11 @@
 #include "Player.h"
 
-Player::Player()
+Player::Player(int numShips)
   {
-    do {
-      cout << "How many ships would you like?" << endl;
-      cin >> numShips;
-      if(numShips > 5 || numShips < 1) //Checks if number of ships is valid
-        {
-          cout << "Please enter a number between 1 and 5" << endl;
-        }
-    } while(numShips > 5 && numShips < 1); //Loops until valid number is given
 
   }
 
-void Player::placeShips(int xPos, int yPos)
+void Player::placeShips(int xPos, int yPos, char orientation)
   {
-    for(int i = 1; i == numShips; i++)
-      {
-        if(i == 1)
-        {
-          cout << "Where would you like to place your ships" << endl;
-        }
-        if(i > 1)
-          {
-            cout << "Place your next ship" << endl;
-          }
-      }
+
   }
