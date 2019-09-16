8b7e5296c1e302a4282bb8a2c57b5642b78b2b3e
hunter0713 <54996583+hunter0713@users.noreply.github.com>
2019-09-16 12:20:24 -0500

    Update gameManager.cpp

--- gameManager.cpp
+++ gameManager.cpp
@@ -1,17 +1,15 @@
 #include "gameManager.h"
-#include <iostream>
 using namespace std;
-gameManager::gameManger()
+gameManager::gameManager()
 {
 start = false;
 gameOver = false;
 run();
 }
-gameManger::~gameManager()
+gameManager::~gameManager()
 {
 
 }
-
 void gameManager::run()
 {
 playerMenu();
@@ -22,43 +20,30 @@ if(start == true)
 }
 void gameManager::playerMenu()
 {
-  int tempShips = 5;
+  int playerChoice=0;
   while(start != true)
     {
+
       cout << "===WELCOME TO BATTLESHIP!====\n========Select Number and Hit Enter to Choose Option===========\n1.) Start Game\n 2.)Rules\n3.)Close Game\n===============================================\n";
       cin >> playerChoice;
       if(playerChoice == 1)
         {
-          cout<<"Game Starting!\n";
-          Player p1(tempShips);
-          Player p2(tempShips);
            start = true;
         }
-     else if(PlayerChoice == 2)
+     else if(playerChoice == 2)
        {
-          cout<<"Current Ruleset: Number of ships = "<<tempShips<<"\nPlease input new number of ships to play (must be between 1 and 5): ";
-          cin>>tempShips;
-          if (tempShips < 1)
-          {
-            tempShips = 1;
-          }
-          else if (tempShips > 5)
-          {
-            tempShips = 5;
-          }
-          cout<<"New Ruleset: "<<tempShips<<" ships per game.";
+
        }
     else if(playerChoice == 3)
        {
-         cout<<"Thanks for playing!";
-         break;
+
        }
     else
       {
         cout << "Invalid Menu Choice! Please Choose Again.\n";
       }
     }
-  gameLoop();
+
 }
 void gameManager::gameLoop()
 {
@@ -66,32 +51,20 @@ void gameManager::gameLoop()
   {
     cout<< "Player One: it's your turn!\n";
     playerTurn(p1);
-    gameOver = p2.isDead();
-    if (gameOver)
-    {
-      gameEnd(p1);
-    }
-    else
-    {
-      cout<< "Player Two: it's your turn!\n";
-      playerTurn(p2);
-    }
-    gameOver = p1.isDead();
-    if (gameOver)
-    {
-      gameEnd(p2);
-    }
-  }
+    cout<< "Player Two: it's your turn!\n";
+    playerTurn(p2);
+}
 }
 void gameManager::playerTurn(Player p)
 {
 int xVal =0;
 int yVal =0;
 char xTemp = ' ';
-cout << "Enter space to fire at: "
+string playerShot = "";
+cout << "Enter space to fire at: ";
 cin >> playerShot;
-xTemp = playerShot.charAt(0);
-yVal = playerShot.charAt(1);
+xTemp = playerShot.at(0);
+yVal = playerShot.at(1);
     switch (xTemp)
 	{
            case 'a':
@@ -122,24 +95,7 @@ yVal = playerShot.charAt(1);
 		cout <<"Choice Out of Bounds!\n";
 		break;
 	}
-p.fireShot(xPos,yPos);
+p.fireShot(xVal,yVal);
 
 
-}
-
-void gameManager::gameEnd(player winner)
-{
-  cout<<"GAME OVER!\n";
-  switch(winner)
-  {
-    case p1;
-      cout<<"Player 1 is the winner!\n";
-      break;
-    case p2;
-      cout<<"Player 2 is the winner!\n";
-      break;
-    default;
-      cout<<"Whoops!  Looks like something's gone wrong.\n";
-      break;
-  }
 }
