6835c443fad83150b3bf802b6c4d571831505884
Zack Khazraeinazmpour <zack_khaz@ku.edu>
2019-09-16 12:29:38 -0500

    Added comments in ship.h and fixed errors in Player.cpp

--- Player.cpp
+++ Player.cpp
@@ -6,7 +6,7 @@ Player::Player(int numShips)
       Ship myShips[numShips];
       for (int i = 1; i <= numShips; i++)
       {
-        //myShips[i-1] = Ship(i); Not sure what this line means/does
+        //myShips[i-1] = Ship(i); Not sure what this line means/does -zack
       }
       ownShips = myShips;
       placeShips();
@@ -17,7 +17,7 @@ void Player::placeShips()
   {
     for(int i = 0; i < numShips; i++)
     {
-      //ownShips[i].placeShip(); placeShip is member of player class not ship class
+      //ownShips[i].placeShip(); placeShip is member of player class not ship class -zack
     }
   }
 
