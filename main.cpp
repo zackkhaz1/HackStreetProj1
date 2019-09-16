94a650326858990fe8eb31fa0f6a9fd390873146
Cobb <hacobb@ku.edu>
2019-09-16 12:06:11 -0500

    fixed last commit

--- main.cpp
+++ main.cpp
@@ -3,21 +3,10 @@
 #include "Ship.h"
 #include <iostream>
 #include "gameManager.h"
-#include <graphics.h>
-#include <conio.h>
 using namespace std;
 
 int main()
   {
     gameManager g1();
     cout << "Test compile" << endl;
-    int gd=DETECT,gm,points[]={320,150,420,300,250,300,320,150};
-
-initgraph(&gd, &gm, "C:\\TC\\BGI");
-
-drawpoly(4, points);
-
-getch();
-closegraph();
-return 0;
   }
