d9e6ac8fcb63560007a1e4f69f77a99dbc1b883e
Abigail Davidow <lenexapaws@gmail.com>
2019-10-07 11:20:15 -0500

    Created animations class to play sunk, hit, miss, and win displays

---
+++ Animations.h
@@ -0,0 +1,16 @@
+#ifndef ANIMATIONS_H
+#define ANIMATIONS_H
+
+#include<iostream>
+using namespace std;
+
+class Animations{
+
+ public:
+   void playHit();
+   void playMiss();
+   void playSunk();
+   void playP1Wins();
+   void playP2Wins();
+};
+#endif
