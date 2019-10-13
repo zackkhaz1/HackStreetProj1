c1d2fd1bd70ebdaed7739e16982df4676ef2f1e7
Jacob <Jacob>
2019-10-12 23:12:57 -0500

    separated scoreBoard.cpp from scoreBoard.h

---
+++ scoreBoard.cpp
@@ -0,0 +1,76 @@
+#include "scoreBoard.h"
+
+int scoreBoard::getTxtSize(){
+	ifstream inFile;
+	string incoming = "";
+	int numScores = 0;
+
+	inFile.open(fileName);
+	if(inFile.is_open())
+		while (inFile >> incoming)
+			numScores++;
+	inFile.close();
+	return (numScores/=2); //this determins the number of scores because each score has two entries in the txt file
+}
+
+void scoreBoard::readOldScores(score newScore){ ///NEEDS IMPLEMENTED
+	ifstream inFile;
+	string oldInitials = "";
+	int oldScoreTotal;
+//	score oldScore;
+	int i = 0;
+	int j = 0;
+	string incoming = "";
+	bool inserted = false;
+	//score *allScores = new score[(getTxtSize()+1)]; //consider using vectors
+	vector<score> allScores(getTxtSize()+1);
+	score tempscore;
+	inFile.open(fileName);
+	if(inFile.is_open()) {
+		incoming = "";
+		while (inFile >> incoming){
+			if(i%2 == 0){
+				oldInitials = incoming;
+			}
+			if(i%2 == 1){
+				oldScoreTotal = stoi(incoming);
+				if((newScore.getScoreTotal() >= oldScoreTotal)&&(!inserted)){
+					allScores.push_back(newScore);
+					inserted = false;
+					j++;
+				}
+				score tempscore(oldInitials, oldScoreTotal);
+				allScores[j] = tempscore;
+			}
+			i++;
+		}
+	}
+	cout << "High Scores:\n";
+	printScoreBoard(allScores);
+
+
+/*	for(int i = 0; i <= getTxtSize(); i++){
+		delete[] allScores[i];
+	}
+	delete[] allScores;
+	*/
+}
+/*	void writeNewScore(score score){ ///MIGHT NEED IMPLEMENTED
+	ofstream myfile;
+	myfile.open (outFile);
+	myfile << "Writing this to a file.\n";
+	myfile.close();
+}
+*/
+
+void scoreBoard::printScoreBoard(vector<score> highScores){
+	ofstream outFile;
+	outFile.open(fileName);
+	if(outFile.is_open()) {
+		for(int i = 0; i <= getTxtSize(); i++){
+	//		cout << highScores[i] << endl;
+	//		outfile << highScores[i] << endl;
+		}
+	outFile.close();
+	}
+}
